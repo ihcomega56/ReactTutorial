8eeef0e55cb27aba4a4bdbaa82f3aa52826e5469
ihcomega56 <ihcomega56@gmail.com>
2016-08-25 21:06:38 +0900

    tutorial8

--- public/scripts/sample.js
+++ public/scripts/sample.js
@@ -1,3 +1,8 @@
+var data = [
+    {id: 1, author: "Pete Hunt", text: "This is one comment"},
+    {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var CommentBox = React.createClass({
     render: function () {
         return (
