f35e7d8f99bb445d4d908f517ed7c77482edaa58
ihcomega56 <ihcomega56@gmail.com>
2016-08-25 21:25:59 +0900

    tutorial13

---
+++ public/scripts/tutorial13.js
@@ -0,0 +1,28 @@
+// jQueryで非同期のリクエストを生成する.
+var CommentBox = React.createClass({
+    getInitialState: function () {
+        return {data: []}
+    },
+    componentDidMount: function() { // コンポーネントが最初にrenderされたあと呼ばれる.
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data}); // 更新.
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data}/>
+                <CommentForm />
+            </div>
+        );
+    }
+});
