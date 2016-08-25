6dd7b67ec35bf901825c1a3ab8e5f1f47ce5a6d0
ihcomega56 <ihcomega56@gmail.com>
2016-08-25 21:42:19 +0900

    tutorial14

--- public/scripts/sample.js
+++ public/scripts/sample.js
@@ -1,24 +1,23 @@
-var data = [
-    {id: 1, author: "Pete Hunt", text: "This is one comment"},
-    {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 var CommentBox = React.createClass({
-    getInitialState: function () {
-        return {data: []}
-    },
-    componentDidMount: function() {
+    loadCommentsFromServer: function () {
         $.ajax({
             url: this.props.url,
             dataType: 'json',
             cache: false,
-            success: function(data) {
+            success: function (data) {
                 this.setState({data: data});
             }.bind(this),
-            error: function(xhr, status, err) {
+            error: function (xhr, status, err) {
                 console.error(this.props.url, status, err.toString());
             }.bind(this)
         });
+    },
+    getInitialState: function () {
+        return {data: []}
+    },
+    componentDidMount: function() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
     },
     render: function () {
         return (
@@ -78,6 +77,6 @@ var Comment = React.createClass({
 });
 
 ReactDOM.render(
-    <CommentBox url="/api/comments" />,
+    <CommentBox url="/api/comments" pollInterval={2000} />,
     document.getElementById('content')
 );
