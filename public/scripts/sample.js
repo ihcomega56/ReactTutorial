d1818e42847a96cc434d4c2bc5964d07b349d8e4
ihcomega56 <ihcomega56@gmail.com>
2016-08-29 02:48:35 +0900

    tutorial17

--- public/scripts/sample.js
+++ public/scripts/sample.js
@@ -57,9 +57,16 @@ var CommentForm = React.createClass({
     handleTextChange: function (e) {
         this.setState({text: e.target.value});
     },
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if (!author || !text) { return; }
+        this.setState({ author: '', text:'' });
+    },
     render: function () {
         return (
-            <form className="commentForm">
+            <form className="commentForm" onSubmit={this.handleSubmit}>
                 <input
                     type="text"
                     placeholder="Your name"
